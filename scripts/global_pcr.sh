6eb3984b3191fe7b372a02fda33180f8d2ad4d44
tccoin <junzhewu@umich.edu>
2023-02-01 01:24:23 +0000

    update params

--- scripts/global_pcr.sh
+++ scripts/global_pcr.sh
@@ -1,1 +1,13 @@
-./build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10
+cd results
+#773 816 10 0
+# flowX13 test_data
+exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 3 0
+
+# identity
+# exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti.yaml /home/link/unified_cvo/results/05_identity.txt 773 816 3 0
+
+
+# exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti_semantic.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10 0
+
+# exec ../build/bin/cvo_global_pcr /kitti/05 /home/junzhewu/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/junzhewu/results/cvo_intensity_img_gpu0_oct25_best/05.txt 593 1350 3
+# exec ../build/bin/cvo_global_pcr /kitti/05 /home/junzhewu/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/junzhewu/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10
