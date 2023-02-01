1a012379e2ef486fd2ad88e553afb6480e24edbc
tccoin <junzhewu@umich.edu>
2023-02-01 01:24:23 +0000

    update params

--- scripts/global_pcr.sh
+++ scripts/global_pcr.sh
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> update params
 cd results
 #773 816 10 0
 # flowX13 test_data
@@ -11,7 +14,11 @@ exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sampl
 # exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti_semantic.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10 0
 
 # exec ../build/bin/cvo_global_pcr /kitti/05 /home/junzhewu/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/junzhewu/results/cvo_intensity_img_gpu0_oct25_best/05.txt 593 1350 3
+<<<<<<< HEAD
 # exec ../build/bin/cvo_global_pcr /kitti/05 /home/junzhewu/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/junzhewu/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10
 =======
 ./build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10
 >>>>>>> update the loop clousure experiment
+=======
+# exec ../build/bin/cvo_global_pcr /kitti/05 /home/junzhewu/cvo_params/cvo_intensity_params_irls_tartan.yaml /home/junzhewu/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 10
+>>>>>>> update params
