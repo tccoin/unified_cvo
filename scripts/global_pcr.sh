131675ede69e7dba0c99e0e61210038e56fa5e57
tccoin <junzhewu@umich.edu>
2023-02-01 02:08:02 +0000

    export pcd with label

--- scripts/global_pcr.sh
+++ scripts/global_pcr.sh
@@ -5,7 +5,8 @@
 cd results
 #773 816 10 0
 # flowX13 test_data
-exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 3 0
+exec ../../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti_semantic.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 3 1
+exec ../../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti.yaml /home/link/unified_cvo/results/cvo_intensity_img_gpu0_oct25_best/05.txt 773 816 3 0
 
 # identity
 # exec ../build/bin/cvo_global_pcr /home/link/unified_cvo/test_data/kitti_05_sample /home/link/unified_cvo/cvo_params/cvo_kitti.yaml /home/link/unified_cvo/results/05_identity.txt 773 816 3 0
